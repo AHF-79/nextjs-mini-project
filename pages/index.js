f7fdbfc9e18f50857e1dd307aa1076b60dc3e1ef
AmirHossein Fattahi <a.fattahi2028@gmail.com>
2023-07-04 19:54:14 +0330

    ready for deployment

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Head from 'next/head';
+import { MongoClient } from 'mongodb';
+
+import MeetupList from '../components/meetups/MeetupList';
+
+function HomePage(props) {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name='description'
+          content='Browse a huge list of highly active React meetups!'
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </React.Fragment>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://AmirHossein:QwYEXzXYlA8h9Ipw@cluster0.vyyhpmq.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
